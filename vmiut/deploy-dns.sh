4c8b8803cfc3ae010a62feab24b81e9e53e839d2
Ethan Robert <ethan.rbrt90@gmail.com>
2026-01-09 15:32:55 +0100

    Ajout script deploy DNS

---
+++ vmiut/deploy-dns.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+# déploiement automatique de db
+
+
+REPO="https://gitlab.univ-lille.fr/baptiste.lavogiez.etu/matrix-scripts/-/archive/main/matrix-scripts-main.tar.gz"
+DIR="/tmp/matrix-scripts-main"
+
+apt-get install -y curl
+
+curl -fL --retry 3 --progress-bar -o /tmp/matrix-scripts.tar.gz "$REPO" 
+tar -xzf /tmp/matrix-scripts.tar.gz -C /tmp 
+cd "$DIR/configuration" 
+
+source ./config.env
+export HOSTNAME=$DNS_HOSTNAME
+export IP_SUFFIX=$DNS_SUFFIX
+
+bash setup-vm.sh 
+
+# phase 2 : installation du service spécialisé
+# on utilise les scripts du dossier associé au service dans le repo git obtenu
+cd ../dns 
+bash install.sh 
+# testing post-installation
+
+bats tests/test.bats && \
+rm -rf /tmp/*
+
+
