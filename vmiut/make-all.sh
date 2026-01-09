bf32477e29dd964aa7e2bdfef25ba85ad101340c
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-01-09 18:44:08 +0100

    make all path

--- vmiut/make-all.sh
+++ vmiut/make-all.sh
@@ -1,5 +1,5 @@
-./make-dns.sh
-./make-db.sh
-./make-matrix.sh
-./make-element.sh
-./make-rproxy.sh
+./scripts/vmiut/make-dns.sh
+./scripts/vmiut/make-db.sh
+./scripts/vmiut/make-matrix.sh
+./scripts/vmiut/make-element.sh
+./scripts/vmiut/make-rproxy.sh
