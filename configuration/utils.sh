0041d8a19ff32af83d8aab7a8418f7305ef5473c
Ethan Robert <ethan.rbrt90@gmail.com>
2026-01-09 11:23:47 +0100

    Création d'un fichier utils pour la verbosité des scripts

---
+++ configuration/utils.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Définition des couleurs
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+# Fonctions de log
+log_info() {
+    echo -e "${BLUE}[INFO]${NC} $1"
+}
+
+log_task() {
+    echo -e "${CYAN}[TASK]${NC} $1"
+}
+
+log_success() {
+    echo -e "${GREEN}[OK]${NC} $1"
+}
+
+log_warning() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${NC} $1" >&2
+}
