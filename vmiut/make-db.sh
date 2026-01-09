32ccc13c708df34fce5b1a9a7489d198f9e5284f
Ethan Robert <ethan.rbrt90@gmail.com>
2026-01-09 11:35:27 +0100

    Correction du script make-db.sh

--- vmiut/make-db.sh
+++ vmiut/make-db.sh
@@ -12,6 +12,5 @@ log_info "Démarrage VM db..."
 vmiut demarrer db
 
 log_info "Exécution du script de déploiement..."
-export SCRIPT=deploy-db.sh # chemin depuis la racine dattier (identique à celui des vboxes)
-vmiut executer db
+SCRIPT=deploy-db.sh vmiut executer db
 log_success "Déploiement terminé"
