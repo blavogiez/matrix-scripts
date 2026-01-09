43d806e80190257573560e3dbbe576f42eb42ec1
Ethan Robert <ethan.rbrt90@gmail.com>
2026-01-09 17:50:22 +0100

    Revert "boucle de ping des VMs"
    This reverts commit 21c1538e78e6e821830413c93c1bf48cec1bfd0c.

--- vmiut/make-rproxy.sh
+++ vmiut/make-rproxy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source "$(dirname "$0")/../configuration/utils.sh"
-source "$(dirname "$0")/../configuration/config.env"
 
 log_info "Suppression ancienne VM rproxy..."
 vmiut supprimer rproxy
@@ -15,12 +14,8 @@ vmiut demarrer rproxy
 
 echo $PWD
 
-TARGET_IP="${IP_PREFIX}.${IP_OCTET3}.${RPROXY_SUFFIX}"
-log_info "Attente de la réponse de la VM ($TARGET_IP)..."
-while ! ping -c 1 $TARGET_IP &> /dev/null; do
-    sleep 1
-done
-log_success "VM accessible !"
+log_info "standby 30S avant démarrage"
+sleep 30
 
 
 
