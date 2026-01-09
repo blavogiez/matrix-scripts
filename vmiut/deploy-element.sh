8c81ffed912cd33ed70365da6dd44a8a78c2fe68
Baptiste Lavogiez <baptiste.lavogiez.etu@univ-lille.fr>
2026-01-09 13:47:07 +0100

    Curl avant get le depot

--- vmiut/deploy-element.sh
+++ vmiut/deploy-element.sh
@@ -8,6 +8,8 @@ set -euo pipefail
 REPO="https://gitlab.univ-lille.fr/baptiste.lavogiez.etu/matrix-scripts/-/archive/main/matrix-scripts-main.tar.gz"
 DIR="/tmp/matrix-scripts-main"
 
+apt-get install -y curl
+
 curl -fL --retry 3 --progress-bar -o /tmp/matrix-scripts.tar.gz "$REPO" && \
 tar -xzf /tmp/matrix-scripts.tar.gz -C /tmp && \
 cd "$DIR/configuration" && \
